0430e5f1ff919ea32e9245a52beece36eb50219b
LONJEZ <clonjez@gmail.com>
2021-06-22 13:22:48 -0700

    Holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,19 +1,21 @@
 #include "holberton.h"
+
 /**
- * main - Print "Holberton" followed by a new line.
- * Description: You are not allowed to include standard libraries.
+ * main - print Holberton, followed by new line
  * Return: 0
  */
+
 int main(void)
 {
-	char holberton[] = "Holberton";
-	int i = 0;
-
-	while (holberton[i] != '\0')
-	{
-		_putchar(holberton[i]);
-		i++;
-	}
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
 
 	return (0);
